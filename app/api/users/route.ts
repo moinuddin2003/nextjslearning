7dafd1fffa1331460405ec89b2c12546666e5109
Muhammad Moinuddin <moinuddin010203@gmail.com>
2026-07-28 15:22:13 +0500

    API with authentication Last task also completed.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -18,16 +18,35 @@ export async function GET(){
     return NextResponse.json(users);
 }
 
-export async function POST(request:Request) {
-    const body = await request.json();
+ import { cookies } from "next/headers";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  if (!token) {
     return NextResponse.json(
-        {
-            status: "Created Successfully",
-            data: body
-        },
-        {
-            statusText:"User Created Successfully",
-            status:201
-        }
-    )
+      {
+        message: "User not authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  return NextResponse.json(
+    {
+      status: "Created Successfully",
+      data: body,
+    },
+    {
+      status: 201,
+    }
+  );
 }
+
+//Postman mein header mein  key mein cookie or Value mein token=ye jo token backend se arha he (Api chalane keliye. THis is the task of API WITH AUTHENTICATION given to us by Sir Ashir)
+ 
